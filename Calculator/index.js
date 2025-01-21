c1349098339abb85151e6f47d4b5ffedb9e541a4
Rishabh Sharma <rishabhsharma8734@gmail.com>
2025-01-21 21:23:46 +0530

    second commit

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,6 +1,5 @@
 let input = document.getElementById('inputBox');
-let buttons  = document.querySelectorAll
-('button');
+let buttons  = document.querySelectorAll('button');
 
 let string = " ";
 let arr = Array.from(buttons);
@@ -11,8 +10,18 @@ arr.forEach(button => {
             input.value = string;
         }
 
-        string += e.target.innerHTML;
-        input.value = string;
+        else if(e.target.innerHTML == 'AC'){
+                        string = "";
+                        input.value = string;
+            }
+                    else if(e.target.innerHTML == 'DEL'){
+                        string = string.substring(0, string.length-1);
+                        input.value = string;
+                    }
+                    else{
+                        string += e.target.innerHTML;
+                        input.value = string;
+                    }
          
     })
 })
